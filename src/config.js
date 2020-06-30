8adb1938ae2dca3f19d470470ff7918439648174
Cawan <cawan.porto@gmail.com>
2020-06-29 23:28:26 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-cawan"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://hyu8auawi3.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_VFNdF67OS",
+      APP_CLIENT_ID: "4jpt2uai8g6u8htruk70eretbt",
+      IDENTITY_POOL_ID: "us-east-1:d5075f8b-e77f-4955-b5ec-1b554a174916"
+    }
+  };
